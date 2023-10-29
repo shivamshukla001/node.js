e26d548e85f79ca7734ae246922d8b897c44eec5
shivamshukla001 <shivamshukla6350@gmail.com>
2023-10-29 09:10:54 +0530

    express basics

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
+const express=require('express');
 
+const app=express();
 
-const http=require('http');
-const data = require('./data');
+app.get('', (req,resp)=>{
+    console.log("data is sent by browser=>>>>>",req.query);
+    resp.send("hello this is a home page");
+})
 
-http.createServer( ( req,resp)=>{
- resp.writeHead(200, {'content-type': 'application/json'});
- resp.write(JSON.stringify(data));
- resp.end();
-}).listen(3002);
+app.get('/about', (req,resp)=>{
+    resp.send("hello this is a about page");
+})
 
+app.get('/help', (req,resp)=>{
+    resp.send("hello this is a help page");
+})
 
-
-
-
-const fs=require('fs');
-
-const input=process.argv;
-console.log(input);
-
-if(input[2]=='add'){
- fs.writeFileSync(input[3],input[4])
-}else if(input[2]=='remove'){
- fs.unlinkSync(input[3])
-}else{
- console.log("invalid input");
-}
--->
-
-
-
-<!-- 
-const fs= require('fs');
-const path=require('path');
-
-const dirpath=path.join(__dirname,'files');
-
-for(i=0; i<5; i++){
-fs.writeFileSync("hello"+i+".txt", "a sample text file");
-}
+app.listen(5200);
