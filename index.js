126a71fccd48c88300a7728f74ea71e36032d0ba
shivamshukla001 <shivamshukla6350@gmail.com>
2023-10-29 09:32:29 +0530

    basics2

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express=require('express');
+const express = require("express");
 
-const app=express();
+const app = express();
 
-app.get('', (req,resp)=>{
-    console.log("data is sent by browser=>>>>>",req.query);
-    resp.send("hello this is a home page");
-})
+app.get("", (req, resp) => {
+  resp.send(`<h1>hello this is a home page</h1>
+  <a href="/about">Go to about page </a>
+  `);
+});
 
-app.get('/about', (req,resp)=>{
-    resp.send("hello this is a about page");
-})
+app.get("/about", (req, resp) => {
+  resp.send(`
+     <input type="text" placeholder="search" value="${req.query.name}"/>
+     <button>Click me</button>    
+     <a href="/">Go to home page </a>
+    `);
+});
 
-app.get('/help', (req,resp)=>{
-    resp.send("hello this is a help page");
-})
+app.get("/help", (req, resp) => {
+  resp.send([
+    {
+      name: "shivam",
+      email: "example.com",
+    },
+    {
+        name: "akash",
+        email: "akash.com",
+      }
+  ]);
+});
 
-app.listen(5200);
+app.listen(1020);
